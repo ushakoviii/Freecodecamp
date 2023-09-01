2113a6faa077567d28b6d2c76619cf5b7ac68033
ushakoviii <ushakov.ii@yandex.ru>
2023-09-01 23:35:25 +0300

    add git

--- Basic JavaScript/basic JS.js
+++ Basic JavaScript/basic JS.js
@@ -18,3 +18,20 @@ let myVar = 87;
 
 // Змініть код лише під цим рядком
 myVar ++ + 1;
+
+const product = 2.0 * 2.5;
+
+const remainder = 11 % 3;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Змініть код лише під цим рядком
+a -= 6;
+b -= 15;
+c -= 1;
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+const myStr = "FirstLine \n \t \\SecondLine \n ThirdLine"; // Change this line
