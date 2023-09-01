69007e1d532e35731a63c0f5cfa72188af2dd37b
ushakoviii <ushakov.ii@yandex.ru>
2023-09-01 17:13:59 +0300

    add git

--- Basic JavaScript/basic JS.js
+++ Basic JavaScript/basic JS.js
@@ -7,3 +7,5 @@ const FCC = "freeCodeCamp"; // Змініть цей рядок
 let fact = "is cool!"; // Змініть цей рядок
 fact = "is awesome!";
 console.log(FCC, fact); // Змініть цей рядок
+
+const sum = 10 + 10;
