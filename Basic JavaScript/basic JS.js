3b59ddade382b0877a05644e0e14014f73d052e4
ushakoviii <ushakov.ii@yandex.ru>
2023-09-01 23:37:08 +0300

    add git

--- Basic JavaScript/basic JS.js
+++ Basic JavaScript/basic JS.js
@@ -34,4 +34,4 @@ c -= 1;
 
 var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
 
-const myStr = "FirstLine \n \t \\SecondLine \n ThirdLine"; // Change this line
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
