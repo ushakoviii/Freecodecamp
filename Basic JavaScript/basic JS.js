c356c980f8ad61bc887d63a141369d65f23a0d44
ushakoviii <ushakov.ii@yandex.ru>
2023-09-01 17:52:37 +0300

    add git

--- Basic JavaScript/basic JS.js
+++ Basic JavaScript/basic JS.js
@@ -9,3 +9,12 @@ fact = "is awesome!";
 console.log(FCC, fact); // Змініть цей рядок
 
 const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+let myVar = 87;
+
+// Змініть код лише під цим рядком
+myVar ++ + 1;
